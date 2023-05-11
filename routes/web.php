1083902a6bfba72e5d0e7f6d3451d8fb91947c05
Mattia Cau <mattiacau1@libero.it>
2023-05-11 20:22:43 +0200

    collegati voli

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [pageCotroller::class, 'homepage'])->name('hompage');
-Route::get('/detail', [pageCotroller::class, 'detail'])->name('detail');
+Route::get('/voli', [pageCotroller::class, 'voli'])->name('voli');
